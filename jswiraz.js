bbcc13a695096fb9acdfcb3596ed4d10d0a73318
Patryk <p171982@gmail.com>
2018-11-11 21:02:01 +0100

    motorbikes categories

---
+++ jswiraz.js
@@ -0,0 +1,20 @@
+function f1(text1){
+	
+	var text1;
+	text1.style.backgroundColor = "red";
+	text1.style.width="100%";
+	text1.style.padding = "145px 5";
+    text1.innerHTML = "<h3>Kazdy zakret </br> przechodzi w prosta</h3>";
+	
+}
+function f2(text1){
+
+    var text1;
+	text1.style.backgroundColor = "black";
+	text1.style.width="100%";
+	text1.style.padding = "145px 5";
+    text1.innerHTML = "<h3>Wiraz...</br>jazda</br>uzaleznia...</h3>";
+
+}
+
+
